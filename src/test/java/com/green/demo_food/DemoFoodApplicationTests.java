b0e3d654895aca36435cfb7abbc628ab111e1bdb
minzion0 <aert12435@gmail.com>
2023-06-15 17:10:26 +0900

    데모 시작

---
+++ src/test/java/com/green/demo_food/DemoFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.demo_food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
